b4fabfe1121d04d21a9bfb7757db68b566e34a18
Ramdhei Lopez Arcila <ramdhei2002@gmail.com>
2026-04-06 16:36:57 -0500

    Add sample data for LINQ class

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using LinqTraining.Data;
+using LinqTraining.Models;
+
+List <Product> products = SampleData.GetProducts(); 
+List <Book> books = SampleData.GetBooks();
+List<Order> orders = SampleData.GetOrders();
+List<Employee> employees = SampleData.GetEmployees();
+
+var employeesDictionary = employees.ToDictionary(e => e.Id);
+
+Console.WriteLine($"Employee with ID 3: {employeesDictionary[3].Name}");
+
+ 
